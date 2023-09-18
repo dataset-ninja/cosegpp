2dbcd35880630aab3e143aab8682058ae42d3a47
vorozhkog <115161827+vorozhkog@users.noreply.github.com>
2023-09-18 17:07:38 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -2,12 +2,12 @@ from typing import Dict, List, Optional, Union
 
 from dataset_tools.templates import (
     AnnotationType,
-    CVTask,
-    Industry,
-    Domain,
-    Research,
-    License,
     Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
 )
 
 ##################################
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "CosegPP"
 PROJECT_NAME_FULL: str = "Cosegmentation for Plant Phenotyping"
+HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -49,29 +50,40 @@ GITHUB_URL: str = "https://github.com/dataset-ninja/cosegpp"
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = {
+    "Images": "https://zenodo.org/record/5117176/files/CosegPP.zip?download=1",
+    "Annotations": "https://zenodo.org/record/5117176/files/CosegPP_groundtruth.zip?download=1",
+}
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = "https://doi.org/10.1371/journal.pone.0257001"
-CITATION_URL: Optional[str] = None
+# If you have more than the one paper, put the most relatable link as the first element of the list
+# Use dict key to specify name for a button
+PAPER: Optional[
+    Union[str, List[str], Dict[str, str]]
+] = "https://doi.org/10.1371/journal.pone.0257001"
+BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
+
+CITATION_URL: Optional[str] = "https://zenodo.org/record/5117176/export/hx"
 AUTHORS: Optional[List[str]] = [
-    "Quiñones R",
-    "Rubi",
-    "Munoz Arriola",
-    "Francisco",
-    "Das Choudhury",
-    "Sruti",
-    "Samal A",
+    "Quiñones, Rubi",
+    "Munoz Arriola, Francisco",
+    "Das Choudhury, Sruti",
+    "Samal. Ashok",
 ]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "University of Nebraska-Lincoln"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://www.unl.edu/"
 
-SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
+# Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
 TAGS: Optional[List[str]] = None
+
+
+SECTION_EXPLORE_CUSTOM_DATASETS: Optional[List[str]] = None
 
 ##################################
 ###### ? Checks. Do not edit #####
@@ -91,6 +103,8 @@ def get_settings():
 
     settings = {
         "project_name": PROJECT_NAME,
+        "project_name_full": PROJECT_NAME_FULL or PROJECT_NAME,
+        "hide_dataset": HIDE_DATASET,
         "license": LICENSE,
         "applications": APPLICATIONS,
         "category": CATEGORY,
@@ -106,15 +120,18 @@ def get_settings():
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
     settings["release_date"] = RELEASE_DATE
-    settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
+    settings["blog"] = BLOGPOST
+    settings["repository"] = REPOSITORY
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
     settings["tags"] = TAGS
+
+    settings["explore_datasets"] = SECTION_EXPLORE_CUSTOM_DATASETS
 
     return settings
